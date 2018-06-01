1b747ef0d896001c2b8caa5720ea205a8e996970
tkkc <tkkc123456789@gmail.com>
2018-06-01 06:29:47 +0000

    user login

--- config/app.php
+++ config/app.php
@@ -165,7 +165,9 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         // 追加
         Collective\Html\HtmlServiceProvider::class,
-
+        // 追加
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
@@ -230,6 +232,8 @@ return [
         // 追加
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        // 追加
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
